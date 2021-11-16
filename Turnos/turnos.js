a5069d70387d46d56f9a75e58722ee700c8244f2
Pablo Ramos <pablo-362@hotmail.com>
2021-11-16 17:39:55 -0300

    modelo reserva turnos

---
+++ Turnos/turnos.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const cbox = document.querySelectorAll(".hover");
+let turno = false;
+
+ for (let i = 0; i < cbox.length; i++) {
+     cbox[i].addEventListener("click", function() {
+      //  document.querySelectorAll('.hover').classList.remove('reservado');
+       cbox[i].innerHTML = 'Reservado';
+       cbox[i].innerHTML = '<button class="cancel">X</button>';
+       cbox[i].classList.add('reservado');
+       cbox[i].classList.remove('hover');
+       turno = true;
+       console.log('turno select');
+     });
+ }
+
+ document.querySelector('#reservar').addEventListener('click', function (){
+   if (turno) {
+    document.querySelector('#reservar').textContent = 'RESERVADO';
+    console.log('Se reservó un turno');
+   }
+  
+});
+
+// Función para eliminar el boton X
+/*
+for (let i = 0; i < cbox.length; i++) {
+  cbox[i].addEventListener("click", function() {
+    turno = false;
+    let myobj = document.querySelector('.cancel');
+    myobj.remove();
+
+  });
+}
+*/
+
